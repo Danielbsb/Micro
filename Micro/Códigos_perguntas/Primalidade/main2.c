ef4257eaad3fc6693a3d1e36399b359c307bdc15
Daniel Carvalho <Daniel Carvalho>
2017-04-17 23:14:35 -0300

    questão 6 (aula 6 e 7)

---
+++ Micro/Códigos_perguntas/Primalidade/main2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+//Função que calcula duplo fatorial.
+int DuploFatorial(unsigned int x){
+    unsigned int Result = 1;
+    int i;
+    for(i = x; i >= 0; i--){
+        if((((x%2) == 0)&& ((i%2) == 0))||(((x%2)!= 0)&& ((i%2) != 0))){
+            Result = Result*i;
+        }
+    }
+    return Result;
+}
+int main()
+{
+    int x;
+    x = DuploFatorial(2);
+    printf("%d",x);
+    return 0;
+}
